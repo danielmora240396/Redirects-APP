d261d8681f582e1215764d1bddf5e958abf35118
danielmora240396 <d.mora240396@gmail.com>
2020-08-18 21:58:43 -0600

    404 page

--- js/404.js
+++ js/404.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
-    $("#header").load("/shared/header.html");
-    $("#footer").load("/shared/footer.html");
+    $("#header").load("https://raw.githubusercontent.com/danielmora240396/Redirects-APP/master/Shared/header.html");
+    $("#footer").load("https://raw.githubusercontent.com/danielmora240396/Redirects-APP/master/Shared/footer.html");
 });
